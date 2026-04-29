4a90fb0ab3a893ba69cc32dd4dc7174572d227fd
Vinni7x <marcos.jansem@discente.ufma.br>
2026-04-29 16:57:29 -0300

    feat:adiocina no controller de curso uma rota para listar cursos por id

--- uninoxus/src/main/java/com/ssp/uninoxus/controllers/CursoController.java
+++ uninoxus/src/main/java/com/ssp/uninoxus/controllers/CursoController.java
@@ -1,6 +1,7 @@
 package com.ssp.uninoxus.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssp.uninoxus.service.CursoService;
 import com.ssp.uninoxus.entities.Curso;
+import com.ssp.uninoxus.service.CursoService;
 
 @RestController
 @RequestMapping(value = "/cursos")
@@ -24,5 +25,10 @@ public class CursoController {
 		return ResponseEntity.ok(lista); 	
 	}
 	 
+	@GetMapping (value = "/idCurso")
+	public ResponseEntity<Optional<Curso>> findById(Long idCurso){
+		Optional<Curso> lista = cursoService.findById(idCurso); 
+		return ResponseEntity.ok(lista); 	
+	}
 
 }
